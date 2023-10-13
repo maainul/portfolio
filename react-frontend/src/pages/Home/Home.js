d661e3e88cb910265f70543169507a1e336cef8f
maainul <mainul080@yahoo.com>
2023-10-13 21:13:05 +0600

    Home Js Style

--- react-frontend/src/pages/Home/Home.js
+++ react-frontend/src/pages/Home/Home.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import './home.css';
+import React from "react";
+import "./home.css";
+import Typewriter from "typewriter-effect";
+import Resume from '../../assets/docs/resume.txt'
+
+
 const Home = () => {
   return (
     <>
-    <h1>Home</h1>
+      <div className="container-fluide home-container">
+        <div className="container home-content">
+          <h1>Hi I'm a</h1>
+          <h2>
+            <Typewriter
+              options={{
+                strings: [
+                  "FullStack Developer !",
+                  "Mern Stack Developer !",
+                  "React Native Developer !",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+            <div className="home-buttons">
+              <button className="btn btn-hire">Hire Me</button>
+              <a className="btn btn-cv" href={Resume} download="your_name.pdf">My Resume</a>
+            </div>
+          </h2>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
