9dfed6a087bd84e592b39e59a4a744e89ffaca11
maainul <mainul080@yahoo.com>
2023-10-15 22:44:25 +0600

    contact

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import Layout from "./components/layout/Layout";
 import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 import Project from "./pages/Project/Project";
 import TechStack from "./pages/TechStack/TechStack";
+import WorkExp from "./pages/WorkExp/WorkExp";
+
+
 
 function App() {
   return (
@@ -12,6 +16,11 @@ function App() {
         <About />
         <TechStack />
         <Project />
+        <WorkExp />
+        <Contact />
+      </div>
+      <div className="footer mb-3 ms-3">
+        <h4 className="text-center">Made With 😎 Md Mainul Hasan 2023</h4>
       </div>
     </>
   );
---
+++ react-frontend/src/pages/WorkExp/WorkExp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './WorkExp.css'
+
+
+
+const WorkExp = () => {
+  return (
+    <div>WorkExp</div>
+  )
+}
+
+export default WorkExp
